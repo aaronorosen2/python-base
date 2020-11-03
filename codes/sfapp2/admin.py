7dc2a7e377ed552b98c11dacc118392f0ad11488
Aaron Rosen <aaronorosen@gmail.com>
2020-11-03 12:22:08 -0800

    expose models and populate

--- codes/sfapp2/admin.py
+++ codes/sfapp2/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question, Choice
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    pass
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
