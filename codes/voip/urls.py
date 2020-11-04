7455d5c1563d6e0ceeab80f5154722840a8bdfd4
Aaron Rosen <aaronorosen@gmail.com>
2020-11-03 17:04:39 -0800

    add twilio_events route and join_conference route

--- codes/voip/urls.py
+++ codes/voip/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('/api_voip/send_sms', views.send_sms_api),
     path('/api_voip/list_sms', views.list_sms_api),
+    path('/api_voip/join_conference', views.join_conference),
+    path('/api_voip/call_status', views.twilio_call_status),
 ]
--- codes/voip/views.py
+++ codes/voip/views.py
@@ -15,3 +15,24 @@ def send_sms_api(request):
 def list_sms_api(request):
     messages = list_sms(request.POST.get("to_number"))
     return JsonResponse({'messages': messages}, safe=False)
+
+
+@csrf_exempt
+def twilio_call_status(request):
+    # XXX first call this which creates an inbound call to source_number
+    print(request.POST)
+
+
+
+@csrf_exempt
+def join_conference(request):
+    # XXX first call this which creates an inbound call to source_number
+    source_number = request.POST.get("source_number")
+    dest_number = request.POST.get("dest_number")
+
+    # Perform this behavior
+    # wait for user to connect and press 1
+
+    # when notified via status callback event from twilio
+    # Then - fire off call to dest_number
+    # Record call entire time
