010812a9394085b14ba3e7373397f541e96525f8
swornim00 <swornimshrestha@Swornims-MacBook-Pro.local>
2020-11-28 18:20:49 +0545

    Lesson API Done

--- codes/courses_api/views.py
+++ codes/courses_api/views.py
@@ -1,8 +1,11 @@
+from django.core.files import File
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import LessonSerializer
 from .models import Lesson
+import json
+import uuid
 
 @api_view(['GET'])
 def apiOverview(request):
@@ -12,24 +15,44 @@ def apiOverview(request):
 
 @api_view(['POST'])
 def lesson_create(request):
-    lessons = Lesson.objects.all()
-    serializer = LessonSerializer(lessons)
-    return Response("This is lessong create")
+    database_data = ""
+    with open("db.json","r") as db:
+        database_data = json.load(db)
+
+    with open("db.json","w") as db:
+        database_data[str(uuid.uuid4())] = request.data
+        json.dump(database_data,db)
+        return Response("Created")
+
 
 @api_view(['GET'])
 def lesson_read(request,pk):
-
-    return Response("This is lessong read")
+    with open("db.json","r") as db:
+        data_json = json.load(db)
+        return Response(data_json[pk])
 
 @api_view(['POST'])
 def lesson_update(request,pk):
+    database_data = ""
 
-    return Response("This is lessong update")
+    with open("db.json","r") as db:
+        database_data = json.load(db)
+
+    with open("db.json","w") as db:
+        database_data[pk] = request.data
+        json.dump(database_data,db)
+        return Response("Updated")
 
 @api_view(['DELETE'])
 def lesson_delete(request,pk):
+    database_data = ""
+    with open("db.json","r") as db:
+        database_data = json.load(db)
 
-    return Response("This is lessong delete")
+    del database_data[pk]
+    with open("db.json","w") as db:
+        json.dump(database_data,db)
+        return Response("Deleted")
 
 # Flashcard API Start
 
