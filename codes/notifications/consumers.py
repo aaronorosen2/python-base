2b43633b6d5dc4954a78f20a22abeb8d5b1b9cf5
Muhammad <hammadishaque602@gmail.com>
2020-11-29 21:24:07 +0500

    User id sends to websocket

--- codes/notifications/consumers.py
+++ codes/notifications/consumers.py
@@ -4,11 +4,27 @@ import json
 class NotificationConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = 'users'
+        # self.user = self.scope["user"]
+        # print(self.user.id)
+        # print(self.channel_name)
+        # print(self.room_name)
+        # print(self.scope['url_route'])
+        # print(self.scope['url_route']['kwargs'])
+        self.group_name = self.scope['url_route']['kwargs']['user_id']
+        print(self.group_name)
         await self.channel_layer.group_add(
             self.room_name,
-            self.channel_name
+            self.group_name
         )
         await self.accept()
+        # print(self.channel_layer.__dict__)
+        # await self.channel_layer.send(
+        #     self.room_name,
+        #     {
+        #         'type': 'users_list',
+        #         'message': self.channel_layer.__dict__
+        #     }
+        # )
 
     async def receive(self, text_data):
         await self.channel_layer.group_send(
@@ -18,7 +34,15 @@ class NotificationConsumer(AsyncWebsocketConsumer):
                 'message': text_data,
             },
         )
-
+    
+    async def users_list(self,event):
+        await self.send_json(
+            {
+                "msg_type": settings.MSG_TYPE_MESSAGE,
+                "channel": event["message"]
+            },
+        )
+    
     async def notification_broadcast(self,event):
         await self.send(text_data=event["message"])
 
--- codes/web/routing.py
+++ codes/web/routing.py
@@ -1,12 +1,27 @@
 from django.urls import path
 from channels.routing import ProtocolTypeRouter, URLRouter
+from django.urls import re_path
 
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 from notifications.consumers import NotificationConsumer
+# from django.core.asgi import get_asgi_application
 
 application = ProtocolTypeRouter({
+    # "http": get_asgi_application(),
     "websocket": 
         URLRouter([
             # URLRouter just takes standard Django path() or url() entries.
-            path("notifications", NotificationConsumer.as_asgi(),name="ws_notifications"),
+            path("notifications/<int:user_id>/", NotificationConsumer.as_asgi(),name="ws_notifications"),
         ]),
 })
+
+# application = ProtocolTypeRouter({
+
+#     "websocket": AuthMiddlewareStack(
+#         URLRouter([
+#             re_path(r"^notifications/$", NotificationConsumer.as_asgi()),
+#         ])
+#     ),
+
+# })
