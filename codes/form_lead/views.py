cd06ad8e8fe38b4485f322a20ddfec45249ef336
handole <denihhandoko@gmail.com>
2020-12-04 20:33:33 +0700

    create model, post & list Lead

--- codes/form_lead/views.py
+++ codes/form_lead/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .models import Lead
+from .serializers import LeadSerializer
+from django.http import Http404
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 # Create your views here.
+
+@api_view(['GET'])
+def lead_list(request):
+    leads = Lead.objects.all()
+    serializer = LeadSerializer(leads, many=True)
+    return Response(serializer.data)
+
+
+class Leadcreate(APIView):
+    def post(self, request, format=None):
+        serializer = LeadSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
