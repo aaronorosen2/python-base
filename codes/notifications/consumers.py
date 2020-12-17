81f5c079c5f367cc7f883bfba2ac537cd9308b80
Muhammad Hammad <mhammad@codexnow.com>
2020-12-17 19:20:09 +0500

    Live Users List, Backstageuser List, Enter Queue When Enters Name of user, Users list shown on notify screen

--- codes/notifications/consumers.py
+++ codes/notifications/consumers.py
@@ -140,6 +140,7 @@ class NotificationConsumerQueue(AsyncWebsocketConsumer):
     user_counter = 0
     user_name = 'Anonymous'
     user_list = []
+    
 
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
@@ -151,17 +152,28 @@ class NotificationConsumerQueue(AsyncWebsocketConsumer):
             self.channel_name
         )
         # enter in back queue
-        redisconn.hset('back',
-                       self.channel_name,
-                       self.room_group_name)
+        redisconn.set('room_name',self.room_group_name)
+        # redisconn.hset('back',
+        #                self.channel_name,
+        #                self.room_group_name)
         # store user details
+        # redisconn.hset('back',
+        #                self.channel_name,
+        #                self.user_name)
         await self.store_user_name(self.channel_name)
         # sends the user list
+        listOfLiveUsers = redisconn.hvals('live')
+        # print(listOfLiveUsers)
+        listOfBackUsers = redisconn.hvals('back')
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'users_list',
-                'users': json.dumps({'users': self.user_list, 'action': 'users_list'}),
+                'users': json.dumps(
+                    {'live_users': listOfLiveUsers,
+                        'action': 'users_list',
+                        'back_users':listOfBackUsers
+                    }),
             },
         )
         await self.accept()
@@ -179,11 +191,21 @@ class NotificationConsumerQueue(AsyncWebsocketConsumer):
             self.user_list.extend(self.user_dictionary.values())
             self.user_channels_details[send_data['user_name']] = self.channel_name
             # await self.print_details(send_data)
+            redisconn.hset('back',
+                       self.channel_name,
+                       send_data['user_name'])
+            listOfLiveUsers = redisconn.hvals('live')
+            listOfBackUsers = redisconn.hvals('back')
             await self.channel_layer.group_send(
                 self.room_group_name,
                 {
                     'type': 'users_list',
-                    'users': json.dumps({'users': self.user_list, 'action': 'users_list'}),
+                    'users': json.dumps(
+                        # {'users': listOfLiveUsers, 'action': 'users_list'}
+                        {'live_users': listOfLiveUsers,
+                        'action': 'users_list',
+                        'back_users':listOfBackUsers
+                        }),
                 },
             )
         elif(send_data['action'] == 'solo'):
@@ -217,12 +239,21 @@ class NotificationConsumerQueue(AsyncWebsocketConsumer):
         del self.user_dictionary[self.channel_name]
         self.user_list.clear()
         self.user_list.extend(self.user_dictionary.values())
+        if(redisconn.hexists('live',self.channel_name)):
+            redisconn.hdel('live', self.channel_name)
+        else:
+            redisconn.hdel('back', self.channel_name)
+        listOfLiveUsers = redisconn.hvals('live')
+        listOfBackUsers = redisconn.hvals('back')
+        dataListOfUsers = {'type': 'users_list',
+                        'users': json.dumps(
+                            {'live_users': listOfLiveUsers,
+                            'action': 'users_list',
+                            'back_users':listOfBackUsers}
+                        ),}
         await self.channel_layer.group_send(
             self.room_group_name,
-            {
-                'type': 'users_list',
-                'users': json.dumps({'users': self.user_list, 'action': 'users_list'}),
-            },
+            dataListOfUsers,
         )
 
     async def store_user_name(self, channel):
@@ -231,7 +262,7 @@ class NotificationConsumerQueue(AsyncWebsocketConsumer):
         self.user_dictionary[channel] = self.user_name
         self.user_list.clear()
         self.user_list.extend(self.user_dictionary.values())
-        redisconn.zadd("backstage", {channel:time.time()+60})
+        # redisconn.zadd("backstage", {channel:time.time()+60})
 
     async def users_list(self,event):
         # print(self.channel_layer)
