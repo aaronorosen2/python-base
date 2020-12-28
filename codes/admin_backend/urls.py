84143f5de2dd4e83bea23a20d691aece41833128
Dheeraj <dheerud69@gmail.com>
2020-12-28 09:31:06 +0530

    added twilio sms

--- codes/admin_backend/urls.py
+++ codes/admin_backend/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api_admin/get_members', views.get_members),
-    path('api_admin/voice', views.voice),
-    path('api_admin/list_calls', views.list_calls),
-    path('api_admin/question_counters', views.get_question_counters),
+    path('/api_admin/get_members', views.get_members),
+    path('/api_admin/voice', views.voice),
+    path('/api_admin/list_calls', views.list_calls),
+    path('/api_admin/question_counters', views.get_question_counters),
 ]
