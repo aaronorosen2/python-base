d5c7d96804455eb79391b8d350256415a13b7255
Aaron Rosen <aaronorosen@gmail.com>
2020-12-30 00:43:10 -0500

    fix up formatting

--- codes/courses_api/models.py
+++ codes/courses_api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Lesson(models.Model):
     lesson_name = models.CharField(max_length=100, blank=True, default='')
     created = models.DateTimeField(auto_now_add=True)
@@ -7,8 +8,9 @@ class Lesson(models.Model):
     def __str__(self):
         return self.lesson_name
 
+
 class FlashCard(models.Model):
-    lesson = models.ForeignKey(Lesson,on_delete=models.CASCADE)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
     lesson_type = models.CharField(max_length=250)
     question = models.CharField(max_length=250)
     options = models.CharField(max_length=250)
@@ -16,13 +18,16 @@ class FlashCard(models.Model):
     image = models.CharField(max_length=250)
     position = models.IntegerField()
 
+
 class UserSession(models.Model):
     session_id = models.CharField(max_length=256, blank=True, null=True)
     created_at = models.DateTimeField(auto_now=True)
-    
+
+
 class UserSessionEvent(models.Model):
     flash_card = models.ForeignKey(FlashCard, on_delete=models.CASCADE)
-    user_session = models.ForeignKey(UserSession, on_delete=models.CASCADE, null=True, blank=True)
+    user_session = models.ForeignKey(UserSession, on_delete=models.CASCADE,
+                                     null=True, blank=True)
     ip_address = models.CharField(max_length=15, blank=True, null=True)
     user_device = models.CharField(max_length=100, blank=True, null=True)
     start_time = models.DateTimeField(auto_now_add=True)
@@ -37,8 +42,10 @@ class UserSessionEvent(models.Model):
     def duration(self):
         return self.end_time - self.start_time
 
+
 class FlashCardResponse(models.Model):
-    user_session = models.ForeignKey(UserSession, on_delete=models.CASCADE, null=True, blank=True)
-    flashcard = models.ForeignKey(FlashCard,on_delete=models.CASCADE)
+    user_session = models.ForeignKey(UserSession, on_delete=models.CASCADE,
+                                     null=True, blank=True)
+    flashcard = models.ForeignKey(FlashCard, on_delete=models.CASCADE)
     answer = models.CharField(max_length=250)
-    signature = models.TextField(blank=True, null=True,default='')
+    signature = models.TextField(blank=True, null=True, default='')
