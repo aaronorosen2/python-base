3df9245fb6ffdbf18fab5373a30d600cb9de14be
Aaron Rosen <aaronorosen@gmail.com>
2021-01-02 13:34:04 -0500

    flashcard response handle update if response exists

--- codes/courses_api/views.py
+++ codes/courses_api/views.py
@@ -231,11 +231,22 @@ def flashcard_response(request):
     flashcard = FlashCard.objects.get(id=flashcard_id)
     user_session = UserSession.objects.get(session_id=session_id)
     print("%s %s %s" % (user_session, flashcard, answer))
+
+    # first check if we have FlashCardResponse
     flashcard_response = FlashCardResponse(
         user_session=user_session,
         lesson=flashcard.lesson,
-        flashcard=flashcard,
-        answer=answer)
+        flashcard=flashcard).first()
+
+    if flashcard_response:
+        # update answer...
+        flashcard_response.answer = answer
+    else:
+        flashcard_response = FlashCardResponse(
+            user_session=user_session,
+            lesson=flashcard.lesson,
+            flashcard=flashcard,
+            answer=answer)
     flashcard_response.save()
     return Response("Response Recorded")
 
