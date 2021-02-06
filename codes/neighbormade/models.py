5785f8142d515ad2e1024e9082f377873f5d7b94
Aaron Rosen <aaronorosen@gmail.com>
2021-02-05 21:10:19 -0800

    neigbhormade data

--- codes/neighbormade/models.py
+++ codes/neighbormade/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Neighborhood(models.Model):
+    name = models.CharField(max_length=250)
+    state = models.CharField(max_length=250)
+    city = models.CharField(max_length=250)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6,
+                                   blank=True, null=True)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6,
+                                    blank=True, null=True)
