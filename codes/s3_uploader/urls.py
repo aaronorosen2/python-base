025139e00a4050b4834aa85a8b947ae57019c573
Aaron Rosen <aaronorosen@gmail.com>
2021-02-16 11:30:08 -0800

    fix up pep8 and extra slash issue

--- codes/s3_uploader/urls.py
+++ codes/s3_uploader/urls.py
@@ -1,35 +1,43 @@
 from django.urls import path, include
 from knox import views as knox_views
 
-from .views import UserRegister, PasswordReset, UserCourses, UserLogin, AllCourses, list_courses, list_courses_auth, \
-    ChangePasswordView, Home, S3SignedUrl, S3Upload, MakeS3FilePublic, UploadRoomLogo, RoomInfoView, RoomVisitor, \
-    RecordingUpload
+
+from .views import UserRegister, PasswordReset, UserCourses, UserLogin, \
+                   AllCourses, list_courses, list_courses_auth, \
+                   ChangePasswordView, Home, S3SignedUrl, S3Upload, \
+                   MakeS3FilePublic, UploadRoomLogo, RoomInfoView, \
+                   RoomVisitor, RecordingUpload
 
 urlpatterns = [
     # User Management and Auth APIs
     path('', Home.as_view(), name='home'),
     path('room_info/', RoomInfoView.as_view(), name='RoomInfoView'),
-    path('upload/record_video/', RecordingUpload.as_view(), name='RecordingUploadView'),
+    path('upload/record_video/', RecordingUpload.as_view(),
+         name='RecordingUploadView'),
     path('upload/room_logo/', UploadRoomLogo.as_view(), name='room_upload'),
     path('room_visitor/', RoomVisitor.as_view(), name='room_upload'),
     path('user/register/', UserRegister.as_view(), name='register'),
     path('user/login', UserLogin.as_view(), name='login'),
     path('user/logout', knox_views.LogoutView.as_view(), name='logout'),
     # path('/user/password_reset', PasswordReset.as_view(), name='reset'),
-    path('user/logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
-    path('user/change-password', ChangePasswordView.as_view(), name='change_password'),
-    path('user/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
-
+    path('user/logoutall/', knox_views.LogoutAllView.as_view(),
+         name='logoutall'),
+    path('user/change-password', ChangePasswordView.as_view(),
+         name='change_password'),
+    path('user/password_reset/', include('django_rest_passwordreset.urls',
+                                         namespace='password_reset')),
     # S3 APIs
-    path('upload/', S3Upload.as_view(), name='upload file'),
-    path('get-s3-url/', S3SignedUrl.as_view(), name='Get s3 signed url'),
-    path('make-public/', MakeS3FilePublic.as_view(), name='Make video public'),
+    path('upload', S3Upload.as_view(), name='upload file'),
+    path('get-s3-url', S3SignedUrl.as_view(), name='Get s3 signed url'),
+    path('make-public', MakeS3FilePublic.as_view(), name='Make video public'),
 
-    # path('/save-video-upload', SaveS3Upload.as_view(), name='save video upload'),
+    # path('/save-video-upload', SaveS3Upload.as_view(),
+    #      name='save video upload'),
 
     # Other APIs using user auth
     path('list/', AllCourses.as_view(), name='courses'),
     path('user/list/', UserCourses.as_view(), name='user_courses'),
     path('list_courses/', list_courses, name='list_courses'),
-    path('list_courses_protected/', list_courses_auth, name='list_courses_protected'),
+    path('list_courses_protected/', list_courses_auth,
+         name='list_courses_protected'),
 ]
