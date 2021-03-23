fb017573d21b3ff663a350042d095517a147a772
tamjeedhur <tamjeedhur@gmail.com>
2021-03-24 02:11:55 +0500

    phone verification resolved

--- codes/staticfiles/script.js
+++
@@ -1,9 +0,0 @@
-$(document).on("click", ".open-AddBookDialog", function () {
-
-    var myBookId = $(this).data('val');
-    console.log(myBookId)
-    const date = document.getElementById("date")
-    date.value= myBookId
-   console.log(date)
-
-});
