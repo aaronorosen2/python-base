c7bc0e916d7fa52a0af9f53990ba10f8c77a2cec
Aaron Rosen <aaronorosen@gmail.com>
2021-04-15 17:28:27 -0700

    update admin view subscription table

--- codes/store/admin.py
+++ codes/store/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import subscription
+
+
+class AdminSubscription(admin.ModelAdmin):
+    list_display = ('source', 'plan_ID', 'BraintreeSubscriptionID')
+
+
+admin.site.register(subscription, AdminSubscription)
