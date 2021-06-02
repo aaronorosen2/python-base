cda361a7d78508411f44a8d149bb504573dd869e
Aaron Rosen <aaronorosen@gmail.com>
2021-06-02 08:37:16 -0700

    Add modeling for initial fitness features

--- codes/sfapp2/models.py
+++ codes/sfapp2/models.py
@@ -125,3 +125,22 @@ class Service(models.Model):
                                    blank=True, null=True)
     longitude = models.DecimalField(max_digits=9, decimal_places=6,
                                     blank=True, null=True)
+
+
+class MemberSession(models.Model):
+    member = models.ForeignKey(to=Member, on_delete=models.CASCADE,
+                               default="", blank=True, null=True)
+    started_at = models.DateTimeField(auto_now_add=True)
+    ended_at = models.DateTimeField()
+
+
+class MemberGpsEntry(models.Model):
+    member_session = models.ForeignKey(to=MemberSession,
+                                       on_delete=models.CASCADE,
+                                       default="", blank=True, null=True)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6,
+                                   blank=True, null=True)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6,
+                                    blank=True, null=True)
+    device_timestamp = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
