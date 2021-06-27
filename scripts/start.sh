c30c41c579f68ddc6778403f195003751da1bf92
Aaron Rosen <aaronorosen@gmail.com>
2021-06-27 14:31:54 -0700

    try make fix this to install qrcode module?

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker-compose -f docker-compose.yml down
-docker-compose -f docker-compose.yml build
+docker-compose -f docker-compose.yml build --no-cache
 docker-compose -f docker-compose.yml up -d
 
