22bbc1b52a4066073097064c1cdd3ddcc5c1b360
Your Name <A R>
2022-08-06 13:24:20 -0700

    add initial gitlab import_github script

---
+++ codes/gitlab/management/commands/import_github.py
@@ -0,0 +1,22 @@
+from github import Github
+from django.core.management.base import BaseCommand
+import os
+
+
+class Command(BaseCommand):
+    help = 'fetch and parse iwlist'
+
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        print("STarting here..")
+        # Github Enterprise with custom hostname
+        g = Github(base_url="https://{hostname}/api/v3",
+                   login_or_token="access_token")
+
+        # Then play with your Github objects:
+        for repo in g.get_user().get_repos():
+            print(repo.name)
+
+
