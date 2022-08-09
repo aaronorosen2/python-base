e581be41ebd8d3afc39d99d1f02c8764e53be6d9
abhi <abhi100100jain@gmail.com>
2022-08-09 19:11:37 +0530

    made fix for gunicorn launch

--- codes/web/routing.py
+++ codes/web/routing.py
@@ -1,13 +1,20 @@
+import django
 from django.urls import path
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import re_path
 
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
+# from channels.auth import AuthMiddlewareStack
+# from django.core.asgi import get_asgi_application
+
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web.settings')
+django.setup()
+
 from notifications.consumers import NotificationConsumer, NotificationConsumerQueue, VstreamConsumer
 from chat.consumers import ChatConsumer
 from chat.middleware import WebSocketJWTAuthMiddleware
-# from django.core.asgi import get_asgi_application
+
 
 application = ProtocolTypeRouter({
     # "http": get_asgi_application(),
@@ -17,8 +24,8 @@ application = ProtocolTypeRouter({
             # path("notifications/<str:room_name>/", NotificationConsumer.as_asgi(),name="ws_notifications"),
             # path("notifications/", NotificationConsumer.as_asgi(),name="ws_notifications"),
             # re_path(r'notifications/(?P<room_name>\w+)/$', NotificationConsumer.as_asgi()),
-            re_path(r'notifications/(?P<room_name>\w+)/$', NotificationConsumerQueue.as_asgi()),
-            re_path(r'vstream/', VstreamConsumer.as_asgi()),
+            # re_path(r'notifications/(?P<room_name>\w+)/$', NotificationConsumerQueue.as_asgi()),
+            # re_path(r'vstream/', VstreamConsumer.as_asgi()),
             re_path(r'msg/(?P<user_id>\w+)/(?P<room_name>\w+)/$', ChatConsumer.as_asgi()),
         ])),
 })
