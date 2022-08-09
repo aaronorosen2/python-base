cae6c3b9a26f32f72586a1ba9432ec2b93ccc821
Your Name <A R>
2022-08-09 11:49:47 -0700

    add in windows driver from Azum

--- codes/utils/wifi.py
+++ codes/utils/wifi.py
@@ -1,5 +1,6 @@
 import os
 import platform
+from subprocess import check_output
 
 
 def scan_networks():
@@ -7,8 +8,7 @@ def scan_networks():
     if os_platform == 'Linux':
         return scan_wifi_linux()
     elif os_platform == 'Windows':
-        # TODO....
-        return []
+        return scan_wifi_windows()
 
 
 def scan_wifi_linux():
@@ -79,3 +79,62 @@ def scan_wifi_linux():
         elif "Mode:" in line:
             network_dict['mode'] = line.split("Mode:", 1)[1]
     return ssids
+
+
+def scan_wifi_windows():
+    '''
+    list of keys follow as:
+        1. SSID 10 : home339E
+        2. Network type
+        3. Authentication
+        4. Encryption
+        5. BSSID 1
+        6 Signal
+        7. Radio type
+        8. Channel
+        9. Basic rates (Mbps)
+        10. Other rates (Mbps)
+
+    :return:
+        Dictionary of the wifi connections:
+    '''
+
+    listWifi = check_output("netsh wlan show networks mode=Bssid", shell=True).decode()
+    print(listWifi)
+    text_file = open("laptop_iwlist_windows_output.txt", "w")
+    n = text_file.write(listWifi)
+    count = 0
+    text_file.close()
+    main_dict = {}
+    count_connection = 0
+    network_dict = {}
+    try:
+        from_file = open('laptop_iwlist_windows_output.txt', 'r')
+        lines = from_file.readlines()
+        # Deleting lines which aren't of use from the executed command
+        del lines[0]
+        del lines[1]
+        del lines[2]
+
+        for line in lines:
+
+            if count == 10:
+                count = 0
+                main_dict['connection' + str(count_connection)] = network_dict
+                count_connection = count_connection + 1
+                network_dict = {}
+            if line != ' ' and line != '':
+                if len(line.split(':')) > 1:
+                    # print(line)
+                    splited = line.split(':')
+                    key, value = splited[0], splited[1]
+                    key = key.strip(' ')
+                    value = value.lstrip(' ')
+                    key = key.rstrip(' ')
+                    value = value.replace("\n", "")
+                    value = value.rstrip(' ')
+                    network_dict[key] = value
+                    count = count + 1
+    except Exception as e:
+        print(e)
+    return main_dict
--- codes/wifi/management/commands/iwlist.py
+++ codes/wifi/management/commands/iwlist.py
@@ -2,21 +2,36 @@ from django.core.management.base import BaseCommand
 from wifi.models import SSIDReading
 import os
 from utils import wifi
+import platform
 
 
 def wifi_scan(training_label):
+    sys_os = platform.system()
 
-    ssids = wifi.scan_networks()
     # now we save all the ssid's to SSIDReading
-    for ssid in ssids:
-        print(ssid)
-        ssid_reading = SSIDReading()
-        ssid_reading.address = ssid['address']
-        ssid_reading.channel = ssid['channel']
-        ssid_reading.quality = ssid['quality']
-        ssid_reading.signal_level = ssid['signal_level']
-        ssid_reading.training_label = training_label
-        ssid_reading.save()
+    if sys_os == 'Linux':
+        ssids = wifi.scan_networks()
+        for ssid in ssids:
+            print(ssid)
+            ssid_reading = SSIDReading()
+            ssid_reading.address = ssid['address']
+            ssid_reading.channel = ssid['channel']
+            ssid_reading.quality = ssid['quality']
+            ssid_reading.signal_level = ssid['signal_level']
+            ssid_reading.training_label = training_label
+            ssid_reading.save()
+    if sys_os == 'Windows':
+        ssids = wifi.scan_networks()
+        size_loop = len(ssids)
+        for i in range(size_loop):
+            ssid_reading = SSIDReading()
+            data_network = ssids['connection'+str(i)]
+            ssid_reading.address = data_network['BSSID 1']
+            ssid_reading.channel = data_network['Channel']
+            ssid_reading.quality = data_network['Signal']
+            #ssid_reading.signal_level = data_network['address']
+            ssid_reading.training_label = training_label
+            ssid_reading.save()
 
 
 class Command(BaseCommand):
