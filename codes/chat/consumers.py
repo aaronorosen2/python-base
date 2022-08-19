21f9ba0fd3d9fbb30aadd8d920536b6cb51621d3
Abhishek Jain <abhi100100jain@gmail.com>
2022-08-19 07:48:46 +0530

    fixed MessageSMS websocket

--- codes/chat/consumers.py
+++ codes/chat/consumers.py
@@ -10,6 +10,7 @@ from rest_framework_simplejwt.tokens import AccessToken, TokenError
 from django.contrib.auth import get_user_model
 from urllib.parse import urlparse, parse_qs
 User = get_user_model()
+# =============================================MessageChannel============================================
 
 class ChatConsumer(AsyncWebsocketConsumer):
 
@@ -60,7 +61,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
         except Channel.DoesNotExist:
             return False
  
-    async def connect(self):       
+    async def connect(self): 
         try:
             parsed_query_string = parse_qs(self.scope["query_string"])
             if parsed_query_string:
@@ -70,15 +71,18 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 if self.user_id:
                     #Adding user in dict
                     self.user_channel_name_list[self.access_token["user_id"]] = self.channel_name
+                try:
                     #Identifying user is in group or not
                     self.room_name =  parsed_query_string.get(b"roomname")[0].decode("utf-8")
                     self.room_exists = await self.get_room(self.user_id,self.room_name)
+                except:
+                    self.close()
                 else:  
 
-                    pass
+                    self.close()
             else:
                 
-                pass
+                self.close()
         except TokenError as e:
             # Socket has not been accepted, so cannot send over it
             # await self.channel_layer.send(
@@ -185,32 +189,27 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def disconnect(self, close_code):
         # Leave room group
         try:
+            # del self.user_dictionary[self.channel_name]
+            # self.user_list.clear()
+            # self.user_list.extend(self.user_dictionary.values())
+            # await self.channel_layer.group_send(
+            #     self.room_group_name,
+            #     {
+            #         'type': 'users_list',
+            #         'users': json.dumps({'users': self.user_list, 'action': 'users_list'}),
+            #     },
+            # )
             await self.channel_layer.group_discard(
-            self.room_group_name,
+            self.room_name,
             self.channel_name
-            )
-            del self.user_dictionary[self.channel_name]
-            self.user_list.clear()
-            self.user_list.extend(self.user_dictionary.values())
-            await self.channel_layer.group_send(
-                self.room_group_name,
-                {
-                    'type': 'users_list',
-                    'users': json.dumps({'users': self.user_list, 'action': 'users_list'}),
-                },
             )
         except:
             self.close()
 
-#=============================================================================================================
+# =============================================MessageUser==================================================
 
 class MessageUserConsumer(AsyncWebsocketConsumer):
 
-    user_dictionary = {}
-    user_channels_details = {}
-    user_counter = 0
-    user_name = 'Anonymous'
-    user_list = []
     user_channel_name_list = {} #dict of all active users
     user_keys_list = [] #dict of all active users
     access_token = 0
@@ -222,21 +221,35 @@ class MessageUserConsumer(AsyncWebsocketConsumer):
     @database_sync_to_async
     def get_user_only(self,user_id):
         try:
-            our_user = User.objects.filter(id=user_id)
-            
+            our_user = User.objects.filter(id=user_id)       
             return our_user.exists()
         except User.DoesNotExist:
             return False
 
     @database_sync_to_async
+    def to_channel_name(self,id):
+            clients_active = Clients.objects.filter(user_id=id).last()  
+            return clients_active.channel_name
+
+    @database_sync_to_async
+    def is_client_active(self,user_id):
+        
+            clients_active = Clients.objects.filter(user_id=user_id)
+            # import pdb; pdb.set_trace()
+            return clients_active.exists()
+
+    @database_sync_to_async
     def clients_add(self,channel,user_id):
-            add = Clients(channel_name=channel,user_id = int(user_id))
-            add.save()
-            return 1 
+            createChannel = {'channel_name':channel,
+            'modified_at' : models.DateTimeField(auto_now=True)
+            }
+            obj, create = Clients.objects.update_or_create(user_id = user_id, defaults=createChannel)
+            return True
+
     @database_sync_to_async
     def clients_delete(self,channel):
-        Clients.objects.filter(channel_name=self.channel_name).delete()
-        return 1 
+        Clients.objects.filter(channel_name=channel).delete()
+        return  True
  
     async def connect(self):       
         try:
@@ -250,14 +263,21 @@ class MessageUserConsumer(AsyncWebsocketConsumer):
                     self.user_channel_name_list[self.access_token["user_id"]] = str(self.channel_name)
                     await self.clients_add(self.channel_name,self.user_id)
                     #Identifying receiver exist
-                    self.receiver_id = int(parsed_query_string.get(b"receiver_id")[0].decode("utf-8"))
-                    self.receiver_id_exists = await self.get_user_only(self.receiver_id)
+
+                    try:
+                        self.receiver_id = int(parsed_query_string.get(b"receiver_id")[0].decode("utf-8"))
+                        self.receiver_id_exists = await self.get_user_only(self.receiver_id)
+                    except:
+                        self.close()
+                        
                 else:  
 
-                    pass
+                    self.close()
+                    
             else:
                 
-                pass
+                self.close()
+                
         except TokenError as e:
             # Socket has not been accepted, so cannot send over it
             # print(self.unidentified_user)
@@ -291,22 +311,25 @@ class MessageUserConsumer(AsyncWebsocketConsumer):
         send_data = json.loads(text_data)
         send_data = json.loads(text_data)
         send_data["User"] = str(self.user_id)
-        if self.user_channel_name_list.get(self.receiver_id):
+        print(text_data)
+        if await self.is_client_active(self.receiver_id):
             await self.channel_layer.send(
-                    self.user_channel_name_list.get(self.receiver_id),
+                    await self.to_channel_name(self.receiver_id),
             {
                 'type': 'notification_to_user',
                 'message': json.dumps(send_data),
             },
             )
-        else:
+        elif await self.is_client_active(self.user_id):
             await self.channel_layer.send(
-                    self.user_channel_name_list.get(self.user_id ),
+                    await  self.to_channel_name(self.user_id),
             {
                 'type': 'error_message',
                 'message': json.dumps({"message":"User is not currently active"})
             },
             )
+        else:
+            pass
 
     @sync_to_async
     def load_message(self, text_data):
@@ -323,10 +346,6 @@ class MessageUserConsumer(AsyncWebsocketConsumer):
             return MessageUserSerializers(message).data
         except Channel.DoesNotExist:
             return False
-
-
-
-
 
 
     async def store_user_name(self, channel):
@@ -361,10 +380,111 @@ class MessageUserConsumer(AsyncWebsocketConsumer):
     async def disconnect(self, close_code):
         # Leave room group
         await self.clients_delete(self.channel_name)
-
+        self.user_channel_name_list.pop(self.user_id)
         self.close()
 
 
+# =============================================MessageSMS============================================
+
+
+class MessageSMSConsumer(AsyncWebsocketConsumer):
+
+    user_channel_name_list = {} #dict of all active users
+    user_keys_list = [] #dict of all active users
+    access_token = 0
+    receiver_id = 0
+    receiver_id_exists = False
+    
+
+    @database_sync_to_async
+    def get_user_only(self,user_id):
+        try:
+            our_user = User.objects.filter(id=user_id)
+            
+            return our_user.exists()
+        except User.DoesNotExist:
+            return False
+
+    @database_sync_to_async
+    def clients_add(self,channel,user_id):
+            createChannel = {'channel_name':channel,
+            'modified_at' : models.DateTimeField(auto_now=True)
+            }
+            obj, create = Clients.objects.update_or_create(user_id = user_id, defaults=createChannel)
+            return True
+    @database_sync_to_async
+    def clients_delete(self,channel):
+        Clients.objects.filter(channel_name=self.channel_name).delete()
+        return 1 
+ 
+    async def connect(self):       
+        try:
+            parsed_query_string = parse_qs(self.scope["query_string"])
+            if parsed_query_string:
+                token = parsed_query_string.get(b"token")[0].decode("utf-8")
+                self.access_token = AccessToken(token)
+                self.user_id = int(self.access_token["user_id"])
+                if self.user_id:
+                    #Adding user in dict
+                    self.user_channel_name_list[self.access_token["user_id"]] = str(self.channel_name)
+                    await self.clients_add(self.channel_name,self.user_id)
+                    #Identifying receiver exist
+                    await self.accept() 
+                    self.user_keys_list = [i for i in self.user_channel_name_list.keys()]
+                    await self.send(text_data =json.dumps(
+                        {
+                            'action': 'users_list',
+                            'users':self.user_keys_list,
+                        },
+                    ))
+                        
+                else:  
+
+                    self.close()
+                    
+            else:
+                
+                self.close()
+                
+        except TokenError as e:
+            # Socket has not been accepted, so cannot send over it
+            # print(self.unidentified_user)
+            # await self.channel_layer.send(
+            #             self.unidentified_user,
+            #     {
+            #         'type': 'error_message',
+            #         'message': json.dumps({"message":f"{e}"})
+            #     },
+            #     )
+            self.close()
+
+    
+
+        
+        
+
+    
+
+    async def users_list(self, event):
+        await self.send(text_data=event["users"])
+
+
+    async def notification_to_user(self, event):
+        await self.send(text_data=event["message"])
+
+    async def error_message(self, event):       
+        await self.send(text_data=json.dumps( event["message"]))
+    
+    
+    # Receive message from room group
+
+
+    async def disconnect(self, close_code):
+        # Leave room group
+        await self.clients_delete(self.channel_name)
+        self.user_channel_name_list.pop(self.user_id)
+
+        self.close()
 
 
 
