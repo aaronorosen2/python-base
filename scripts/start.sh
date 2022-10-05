875b75a80283b1641c8c6daa1dec0210e06e707b
AR <a@dreampotential.org>
2022-10-05 22:33:10 +0000

    try this one

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,5 +6,5 @@ docker-compose -f docker-compose.yml down
 docker-compose -f docker-compose.yml build
 export db_host=`curl http://169.254.169.254/latest/meta-data/public-ipv4`
 db_host=$db_host docker-compose -f docker-compose.yml up -d
-docker-compose -f docker-compose.yml logs -f
+# docker-compose -f docker-compose.yml logs -f
 
