63da0dc2f1e02637634d4aa8c4660f3ff1209478
Ubuntu <ubuntu@ip-172-31-19-135.us-east-2.compute.internal>
2022-10-05 21:41:52 +0000

    Add updates from Eric

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-docker-compose -f docker-compose.yml down 
+
+set -x
+
+docker-compose -f docker-compose.yml down
 docker-compose -f docker-compose.yml build
-docker-compose -f docker-compose.yml up -d 
+export db_host=`curl http://169.254.169.254/latest/meta-data/public-ipv4`
+db_host=$db_host docker-compose -f docker-compose.yml up -d
 docker-compose -f docker-compose.yml logs -f
 
